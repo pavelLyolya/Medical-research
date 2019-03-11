e98345f50de279839f679c9c8db0e79a04c96f79
Pavel Lyolya <teenager9884@gmail.com>
2019-03-11 13:15:25 +0300

    perf: add TeamsHeader

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header/Header';
 import Footer from './Footer';
 import TableStandingsContainer from '../containers/TableStandingsContainer';
+import Teams from './Teams/Teams';
 
 const App = () => (
     <Router>
@@ -11,6 +12,7 @@ const App = () => (
             <main>
                 <Route exact path='/' />
                 <Route path='/table' component={TableStandingsContainer} />
+                <Route path='/teams' component={Teams} />
             </main>
             <Footer />
         </React.Fragment>
