6e8754fe080c4554325ce0ad9511c0badc944c74
Pavel Lyolya <teenager9884@gmail.com>
2019-03-12 12:01:45 +0300

    feat: add links instead spans

--- src/app/components/Teams/TeamsList.js
+++ src/app/components/Teams/TeamsList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const TeamItem = ({ name, shortName, imgURL }) => (
+const TeamItem = ({
+    teamId, name, shortName, imgURL,
+}) => (
     <div className='teamItem'>
         <div className='crestImg'>
             <img src={imgURL} />
         </div>
         <div className='teamInfo'>
-            <span className='teamName'>{name}</span>
+            <Link to={`/teams/${teamId}`} className='teamName'>{name}</Link>
             <span className='teamShortName'>short name: {shortName}</span>
             <button>Add team to favorites</button>
         </div>
@@ -15,6 +18,7 @@ const TeamItem = ({ name, shortName, imgURL }) => (
 );
 
 TeamItem.propTypes = {
+    teamId: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     shortName: PropTypes.string.isRequired,
     imgURL: PropTypes.string.isRequired,
@@ -25,6 +29,7 @@ const TeamsList = ({ teamsArray }) => (
         {teamsArray.map(item => (
             <TeamItem
                 key={item.id}
+                teamId={item.id}
                 name={item.name}
                 shortName={item.shortName}
                 imgURL={item.imgURL}
