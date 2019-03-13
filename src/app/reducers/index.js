d24a88a543b79242108c156fedc2e3f9d97a726b
Pavel Lyolya <teenager9884@gmail.com>
2019-03-13 16:42:40 +0300

    refactor: delete favoriteTeams from state

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import favoriteTeams from './favoriteTeams';
 import leagues from './leagues';
 import activeLeagueId from './activeLeagueId';
 import activePageName from './activePageName';
 
 export default combineReducers({
-    favoriteTeams,
     activeLeagueId,
     activePageName,
     leagues,
