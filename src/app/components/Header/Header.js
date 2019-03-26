27a6b87db1bd4cdc75fc98b9ff3ff0c4ef3893d2
Pavel Lyolya <teenager9884@gmail.com>
2019-03-26 16:40:54 +0300

    perf: delete signing component

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Logo from './Logo';
 import NavigationContainer from '../../containers/NavigationContainer';
-import Signing from './Signing';
+// import Signing from './Signing';
 import '../../../css/Header.scss';
 
 const Header = () => (
     <header className='appHeader'>
         <Logo />
         <NavigationContainer />
-        <Signing />
+        {/* <Signing /> */}
     </header>
 );
 
