3da7fc72a1566799f1d3623e9d2961f3c83cbb32
Pavel Lyolya <teenager9884@gmail.com>
2019-03-27 17:18:43 +0300

    refactor: delete unnecessary comments

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Logo from './Logo';
 import NavigationContainer from '../../containers/NavigationContainer';
-// import Signing from './Signing';
 import '../../../css/Header.scss';
 
 const Header = () => (
     <header className='appHeader'>
         <Logo />
         <NavigationContainer />
-        {/* <Signing /> */}
     </header>
 );
 
