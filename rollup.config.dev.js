4e246fd1ef7bd0d25b20c165cea248dd5557705c
Pavel Lyolya <teenager9884@gmail.com>
2019-04-18 17:04:24 +0300

    multipoint entry

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,3 +1,5 @@
+import '@babel/polyfill';
+import multiEntry from 'rollup-plugin-multi-entry';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
@@ -9,15 +11,20 @@ import htmlTemplate from 'rollup-plugin-generate-html-template';
 import json from 'rollup-plugin-json';
 
 export default {
-    input: './src/app/index.js',
+    input: [
+        '@babel/polyfill',
+        './src/app/index.js',
+    ],
     output: {
         file: './dist/bundle.js',
         format: 'iife',
         sourcemap: true,
+        exports: 'named',
         globals: {
             react: 'React',
             'react-dom': 'ReactDOM',
             'react-is': 'reactIs',
+            'prop-types': 'PropTypes',
         },
     },
     external: [
@@ -25,8 +32,10 @@ export default {
         'react-dom',
         'react-is',
         'styled-components',
+        'prop-types',
     ],
     plugins: [
+        multiEntry(),
         babel({
             exclude: 'node_modules/**',
         }),
@@ -37,7 +46,7 @@ export default {
             'process.env.NODE_ENV': JSON.stringify('development'),
         }),
         commonjs({
-            include: 'node_modules/**',
+            // include: 'node_modules/**',
             // namedExports:
             // {
             //     './node_modules/react/react.js':
